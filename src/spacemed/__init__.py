64b5ff6666067827ac1a8fb2cf40285edd54788d
Sammy Chen <shan-shan.chen@charite.de>
2026-05-09 11:37:01 +0200

    fix command line program

--- src/spacemed/__init__.py
+++ src/spacemed/__init__.py
@@ -1,6 +1,6 @@
-from .__version__ import __version__  # noqa: F401
-from .load_pulse import readPulse  # noqa: F401
-from .find_peaks import findPeaks  # noqa: F401
-from .cal_hr import calHR  # noqa: F401
-from .plot_hr import plotHR  # noqa: F401
-from .mri import fmriCross  # noqa: F401
+from .__version__ import __version__  # noqa
+from .load_pulse import readPulse  # noqa
+from .find_peaks import findPeaks  # noqa
+from .cal_hr import calHR  # noqa
+from .plot_hr import plotHR  # noqa
+from .mri import fmriCross  # noqa
