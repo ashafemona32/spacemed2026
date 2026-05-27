bcf3492d13fcdc13cd84d50d4f812e07debef8f5
Sammy Chen <shan-shan.chen@charite.de>
2026-05-27 23:20:50 +0200

    adjust fmri.sh with environment activation

--- fmri.sh
+++ fmri.sh
@@ -8,7 +8,11 @@
 #SBATCH --output=logs/plotfmri_%j.out
 #SBATCH --array=1-32
 
-#the program we are running
+# 1. activate environment
+source /opt/miniforge/etc/profile.d/conda.sh
+conda activate spacemed26
+
+# 2.the program we are running
 echo "starting job ${SLURM_ARRAY_TASK_ID}"
 sm_plot_fmri "fmri-data/3_fMRI_TR2sec_3mm_3min.nii" -s ${SLURM_ARRAY_TASK_ID}  -o "fmri-data/fMRI_slice_${SLURM_ARRAY_TASK_ID}.png"
 echo done
